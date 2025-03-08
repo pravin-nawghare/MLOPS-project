cc96358eb4d9ce28ebd44b6a6d60b788a6e02f16
pravin-nawghare <nawgharepraveen@gmail.com>
2025-03-08 18:00:01 +0530

    Mlflow code added

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -6,6 +6,7 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 import pickle
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 from src.logger.logging import logging
 from src.exception.exception import CustomeException
@@ -22,11 +23,40 @@ class ModelEvaluationConfig:
 class ModelEvaluation:
     
     def __init__(self):
-        pass
+        logging.info("Evaluation started")
 
-    def initiate_model_evaluation(Self):
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual ,pred)
+        r2 = r2_score(actual , pred)
+        logging.info("Evaluation metrics captured")
+        return rmse, mae, r2
+    def initiate_model_evaluation(self, train_array, test_array):
         try:
-            pass
+            X_test, y_test = (test_array[:,:-1], test_array[:,-1])
+
+            model_path = os.path.join("artifacts", "model.pkl")
+            model = load_object(model_path)
+
+            mlflow.set_registry_uri("") # uri of cloud location where model is to be registered
+            logging.info("Model is registred")
+
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            with mlflow.start_run():
+                predictions = model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predictions) 
+
+                #mlflow.log_param()
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("mae", mae)
+                mlflow.log_metric("r2_score", r2)
+
+                if tracking_url_type_store != "file":
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="first_model")
+                else:
+                    mlflow.sklearn.log_model(model, "model")
 
 
         except Exception as e:
@@ -34,7 +64,7 @@ class ModelEvaluation:
         
         
         
-        pass
+        
     
     
-    pass
+    
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.components.model_evaluation import ModelEvaluation
 
 def run_pipeline():
     obj = DataIngestion()
@@ -16,4 +17,9 @@ def run_pipeline():
     train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
     model_trainer = ModelTrainer()
-    model_trainer_obj = model_trainer.initiate_model_trainer(train_arr, test_arr)
+    #model_trainer_obj = 
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
+
+    model_evaluation = ModelEvaluation()
+    #model_evaluation_obj = 
+    model_evaluation.initiate_model_evaluation(train_arr, test_arr)
